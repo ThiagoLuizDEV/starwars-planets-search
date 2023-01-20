96f8194ca9849e0c8c7cfd327af32efbbc99de30
Thiago <thiago-470@outlook.com>
2023-01-20 18:43:24 -0300

    requisito 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
+  const [getPlanets, setGetPlanets] = useState([]);
+
+  useEffect(() => {
+    const getFetchApi = async () => {
+      const response = await fetch('https://swapi.dev/api/planets');
+      const data = await response.json();
+      setGetPlanets(data.results.filter((remove) => delete remove.residents));
+    };
+    getFetchApi();
+  }, []);
+
   return (
-    <span>Hello, App! </span>
+    <div>
+      <Table getPlanets={ getPlanets } />
+    </div>
   );
 }
 
